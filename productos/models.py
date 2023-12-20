701fd5b94de75a3583a0724c515b4f73ec9a4945
fabri2605 <fabridp77@gmail.com>
2023-12-20 12:04:29 -0300

    c-144

---
+++ productos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.TextField(max_length=255)
+
+class Producto(models.Model):
+    nombre = models.TextField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
